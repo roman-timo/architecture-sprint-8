6bfae1b5da2ffa94ac1d361fd6a6f9ce19f0d843
Roman Timo <93447095+roman-timo@users.noreply.github.com>
2025-04-07 12:54:01 +0300

    added pkceMethod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,10 @@ const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider 
+      authClient={keycloak}
+      initOptions={{ pkceMethod: 'S256' }}
+      >
       <div className="App">
         <ReportPage />
       </div>
